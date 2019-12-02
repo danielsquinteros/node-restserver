964bc2208552995cb8c0d01f3c3a7b43a8a1d3e4
Daniel Quinteros <danielsquinteros@gmail.com>
2019-12-02 16:49:56 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:UIq4EgKYUFCVUxUs@cluster0-vpw98.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
