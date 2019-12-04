7c7209835b60e5ce0099aacc27270689c8b22386
Daniel Quinteros <danielsquinteros@gmail.com>
2019-12-04 13:24:25 -0300

    Login normal

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken');
+
+
+// ======================
+// Verificar Token
+// ======================
+
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token');
+    //.get se obtienen o leen los headers
+
+    jwt.verify( token, process.env.SEED, (err, decoded) => {
+
+        if( err ){
+            return res.status(400).json({
+                ok: false,
+                err: 'Token no válido'
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    })
+}
+
+// ======================
+// Verificar AdminRol
+// ======================
+
+let verificaAdmin_Role = (req, res, next) => {
+    let usuario = req.usuario;
+
+    if( usuario.role === 'ADMIN_ROLE'){
+        next();
+    } else {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    }
+}
+
+
+
+
+module.exports = {
+    verificaToken,
+    verificaAdmin_Role
+}
