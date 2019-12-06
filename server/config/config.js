45b59b21d5065b12496396199ca33fe05c5a6172
Daniel Quinteros <danielsquinteros@gmail.com>
2019-12-06 15:30:08 -0300

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -48,3 +48,10 @@ if (process.env.NODE_ENV === 'dev'){
 }
 
 process.env.URLDB = urlDB;
+
+
+// =================================
+// Google Client ID
+// =================================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '933576229679-num9s0jpamfirlfccpthh9k9eb7j01ei.apps.googleusercontent.com';
